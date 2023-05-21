e321a0f49745a9419c1c48c1dea5aaa4d12d3fae
Crystal Vega <110161341+crystalvega@users.noreply.github.com>
2023-05-21 23:03:41 +0500

    update

--- App Links API/server.js
+++ App Links API/server.js
@@ -2,7 +2,13 @@
 var http = require('http');
 var port = process.env.PORT || 1337;
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World!');
-}).listen(port);
+fs.readFile('public/index.html', function (err, html) {
+
+    if (err) throw err;
+
+    http.createServer(function (request, response) {
+        response.writeHeader(200, { "Content-Type": "text/html" });
+        response.write(html);
+        response.end();
+    }).listen(PORT);
+});
